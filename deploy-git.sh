16ea4c15997a1b8e7be8a83af67c17068d57baa2
Andrew <anxsaiyuki@gmail.com>
2016-09-27 17:22:10 -0700

    modified deploy-git shell script

--- deploy-git.sh
+++ deploy-git.sh
@@ -2,6 +2,8 @@ git add -A
 git status
 echo "Please enter commit message: "
 read input_message
+echo "============================"
 echo "You entered: $input_message"
+echo "============================"
 git commit -m "$input_message"
 git push -u origin master
